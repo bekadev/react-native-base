4a46a8227e9d0139ab1fc7d2386fd1465afdf966
beka.dev <bigsamsa3@mail.ru>
2023-04-01 00:12:17 +0600

    new project for RN

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ActivityIndicator, Button, StyleSheet, Text, View, Image, Pressable, Alert} from 'react-native';
+
+const img = require('./assets/adaptive-icon.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={stylesFlex.container}>
+        <View style={{flex: 1, backgroundColor: '#fff'}}/>
+        <View style={[stylesFlex.container, stylesFlex.color]}>
+          <Text style={stylesFlex.text}>Hi</Text>
+          <Button
+              onPress={() => {}}
+              title="Learn More"
+              color="#fff"
+              accessibilityLabel="Learn more about this purple button"
+          />
+          <Image
+              style={styles.img}
+              source={img}
+          />
+          <ActivityIndicator size={'large'} color={'red'}/>
+          <Pressable hitSlop={30} onPress={() => {
+            Alert.alert('titile')
+            }
+          }>
+            <Text>presss</Text>
+          </Pressable>
+        </View>
+        <View style={{flex: 1, backgroundColor: '#fff'}}/>
+      </View>
     </View>
   );
 }
@@ -13,8 +37,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#9393ea',
   },
+  img: {
+    width: 50,
+    height: 50
+  }
+});
+const stylesFlex = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  color: {
+    backgroundColor: '#3f3f3f'
+  },
+  text: {
+    fontSize: 50,
+    color: '#fff',
+    textAlign: 'center',
+  }
 });
