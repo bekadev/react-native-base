aca9e6ccaf3df6456e4f00df00a0c714f7a28b57
beka.dev <bigsamsa3@mail.ru>
2023-04-02 23:19:04 +0600

    rn: create block phone

--- App.tsx
+++ App.tsx
@@ -1,35 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import {ActivityIndicator, Button, StyleSheet, Text, View, Image, Pressable, Alert} from 'react-native';
+import {Dimensions} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  ListRenderItem
+} from 'react-native';
 
-const img = require('./assets/adaptive-icon.png')
+
+const images = [
+  require('./assets/image1.png'),
+  require('./assets/image2.png'),
+  require('./assets/image3.png'),
+  require('./assets/image4.png'),
+  require('./assets/image5.png'),
+  require('./assets/image6.png'),
+]
+
+const titles = [
+  'Apple iPhone 13 \n128GB Blue',
+  'Apple iPhone 14 Pro 128GB Space Black',
+  'Apple iPhone 12 128GB Purple',
+  'Apple iPhone SE 128GB 2022 Midnight',
+  'Apple iPhone 13 512GB Midnight',
+  'Apple iPhone 14 Pro Max 256GB Purple'
+]
+const prices = [999, 1199, 799, 599, 899, 1199]
+
+
+type ItemType = {
+  id: number
+  title: string
+  price: number
+  image: any
+}
+
+const fakeData: ItemType[] = [...Array(12)].map((_, index) => ({ id: index + 1,
+  title: titles[index % titles.length],
+  price: prices[index % prices.length],
+  image: images[index % images.length],
+}))
 
 export default function App() {
+  const renderItem: ListRenderItem<ItemType> = ({item}) => {
+    return <View style={styles.itemPhone}>
+      <Image
+          style={styles.phoneImage}
+        resizeMode={'contain'}
+        source={item.image}
+      />
+      <View style={styles.infoPhone}>
+        <Text style={styles.phoneName}>{item.title}</Text>
+        <Text>$ {item.price}</Text>
+      </View>
+    </View>
+  }
   return (
     <View style={styles.container}>
-      <View style={stylesFlex.container}>
-        <View style={{flex: 1, backgroundColor: '#fff'}}/>
-        <View style={[stylesFlex.container, stylesFlex.color]}>
-          <Text style={stylesFlex.text}>Hi</Text>
-          <Button
-              onPress={() => {}}
-              title="Learn More"
-              color="#fff"
-              accessibilityLabel="Learn more about this purple button"
-          />
-          <Image
-              style={styles.img}
-              source={img}
-          />
-          <ActivityIndicator size={'large'} color={'red'}/>
-          <Pressable hitSlop={30} onPress={() => {
-            Alert.alert('titile')
-            }
-          }>
-            <Text>presss</Text>
-          </Pressable>
-        </View>
-        <View style={{flex: 1, backgroundColor: '#fff'}}/>
-      </View>
+      <StatusBar style={'light'}/>
+      <FlatList
+          data={fakeData}
+          renderItem={renderItem}
+          numColumns={2}
+          contentContainerStyle={{paddingHorizontal: 16}}
+          columnWrapperStyle={{justifyContent: 'space-between'}}
+      />
     </View>
   );
 }
@@ -37,24 +74,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#9393ea',
+    backgroundColor: '#fff',
   },
-  img: {
-    width: 50,
-    height: 50
-  }
-});
-const stylesFlex = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column'
+  itemPhone : {
+    backgroundColor: '#fff',
+    width: (400 - 16 * 2) / 2 - 8,
+    marginVertical: 6,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.4)',
+    borderRadius: 5,
+    paddingTop: 12
   },
-  color: {
-    backgroundColor: '#3f3f3f'
+  phoneImage: {
+    width: (400 - 16 * 2) / 2 - 8,
+    height: (400 - 16 * 2) / 2 - 8,
   },
-  text: {
-    fontSize: 50,
-    color: '#fff',
-    textAlign: 'center',
+  infoPhone: {
+    marginTop: 19,
+    marginBottom: 22,
+    paddingLeft: 12,
+    paddingRight: 15,
+  },
+  phoneName: {
+    fontSize: 12,
+    fontWeight: '500',
+    lineHeight: 16,
   }
 });
