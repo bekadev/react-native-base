1c95073113ed6258a19b76dd9817a1b50a907377
beka.dev <bigsamsa3@mail.ru>
2023-04-04 23:09:02 +0600

    rn: finish task flatlist

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {Dimensions} from "react-native";
+import {Dimensions, Platform, Pressable} from "react-native";
 import {
   StyleSheet,
   Text,
@@ -8,6 +8,8 @@ import {
   FlatList,
   ListRenderItem
 } from 'react-native';
+import {BasketIcon} from "./SvgIcons/BasketIcon";
+import {BurgerMenu} from "./SvgIcons/BurgerMenu";
 
 
 const images = [
@@ -53,7 +55,12 @@ export default function App() {
       />
       <View style={styles.infoPhone}>
         <Text style={styles.phoneName}>{item.title}</Text>
-        <Text>$ {item.price}</Text>
+        <View style={styles.containerPhonePrice}>
+          <Text style={styles.phonePrice}>$ {item.price}</Text>
+          <Pressable>
+              <BasketIcon />
+          </Pressable>
+        </View>
       </View>
     </View>
   }
@@ -66,15 +73,53 @@ export default function App() {
           numColumns={2}
           contentContainerStyle={{paddingHorizontal: 16}}
           columnWrapperStyle={{justifyContent: 'space-between'}}
+          ListHeaderComponent={Header}
+          ListHeaderComponentStyle={styles.header}
+          stickyHeaderIndices={[0]}
+          ListFooterComponent={Footer}
+          ListFooterComponentStyle={styles.footer}
+          ListEmptyComponent={Empty}
       />
     </View>
   );
 }
 
+const  Header = () => {
+  return (
+       <View style={styles.heaerContent}>
+         <Pressable>
+           <BurgerMenu />
+         </Pressable>
+         <Pressable>
+           <BasketIcon colorFill={'#fff'}/>
+         </Pressable>
+       </View>
+  )
+}
+
+const Footer = () => {
+  return (
+      <View style={styles.footerContent}>
+        <Text style={styles.footerText}>
+          2023 beka.dev All rights reserved
+        </Text>
+      </View>
+  )
+}
+
+const Empty = () => {
+  return (
+      <View style={styles.emptyContent}>
+        <Text style={styles.emptyTitle}>Oops! This place looks empty</Text>
+        <Text style={styles.emptySubTitle}>Oops! This place looks empty</Text>
+      </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#969090',
   },
   itemPhone : {
     backgroundColor: '#fff',
@@ -83,7 +128,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'rgba(0,0,0,0.4)',
     borderRadius: 5,
-    paddingTop: 12
+    paddingTop: 12,
   },
   phoneImage: {
     width: (400 - 16 * 2) / 2 - 8,
@@ -99,5 +144,57 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: '500',
     lineHeight: 16,
+  },
+  containerPhonePrice: {
+    marginTop: 17,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: "space-between"
+  },
+  phonePrice: {
+    fontWeight: '400',
+    lineHeight: 12,
+    fontSize: 12
+  },
+  header: {
+    borderWidth: 1,
+    marginHorizontal: -18,
+    paddingTop: Platform.OS === 'ios' ? 50 : 20,
+    backgroundColor: '#21201E',
+    marginBottom: 19
+  },
+  heaerContent: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 12
+  },
+  footer: {
+    marginHorizontal: -18,
+    backgroundColor: '#21201E'
+  },
+  footerContent: {
+    paddingTop: 15,
+    paddingBottom: 25,
+    alignItems: 'center'
+  },
+  footerText: {
+    color: '#fff'
+  },
+  emptyContent: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  emptyTitle: {
+    fontWeight: '500',
+    fontSize: 20,
+    lineHeight: 24
+  },
+  emptySubTitle: {
+    fontWeight: '400',
+    fontSize: 14,
+    lineHeight: 24
   }
 });
